fb64f38855affaf3b1bdcc69baa01513641d365c
Ben Sparks <bsparks@unicon.net>
2014-12-12 10:46:23 -0700

    view headers in logs

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ http.createServer(function (req, res) {
     }
     pathname = url.parse(req.url).pathname;
 
+    console.log('headers: ', req.headers);
     console.log('hostname: ', hostname);
     console.log('pathname: ', pathname);
 
