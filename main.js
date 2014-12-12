f8ca19a617a49b66f2a5c2489ef2d7de36242143
Ben Sparks <bsparks@unicon.net>
2014-12-12 10:28:54 -0700

    some case of no host caused process to crash

--- main.js
+++ main.js
@@ -4,12 +4,19 @@ var http = require('http'),
     url = require('url');
 
 http.createServer(function(req, res) {
-    var hostname = req.headers.host.split(":")[0];
-    var pathname = url.parse(req.url).pathname;
+    var hostname, pathname;
 
-    console.log(hostname);
-    console.log(pathname);
+    if(!req.headers.host) {
+        console.log('no host in headers', req.headers);
+    } else {
+        hostname = req.headers.host.split(":")[0];
+    }
+    pathname = url.parse(req.url).pathname;
 
+    console.log('hostname: ', hostname);
+    console.log('pathname: ', pathname);
+
+    // TODO: pull ports from config
     switch(hostname)
     {
         case 'dev.server.ironbane.com':
